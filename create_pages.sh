a11a52ff3744388d98e0a2eff934cab2e160a831
Diegoormond <diegomaiaofficial@gmail.com>
2026-01-06 20:49:14 -0300

    Delete create_pages.sh

--- create_pages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Script to create all missing pages for ejetaragua.com
-# Run this after verifying the template works
-
-echo "Creating remaining pages..."
-echo "This is a template - actual implementation will be done programmatically"
